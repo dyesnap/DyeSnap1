ca1b62c94655d0caacbcfeb618b22eee4937d7b9
dyesnap <dyesnap@gmail.com>
2024-11-26 09:15:28 +0100

    dye

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -3,35 +3,4 @@ import { Scene, Storyboard } from 'utopia-api'
 import { App } from '../src/app'
 import { Playground } from '../src/playground'
 
-export var storyboard = (
-  <Storyboard>
-    <Scene
-      id='playground-scene'
-      commentId='playground-scene'
-      style={{
-        width: 700,
-        height: 759,
-        position: 'absolute',
-        left: 212,
-        top: 128,
-      }}
-      data-label='Playground'
-    >
-      <Playground style={{}} />
-    </Scene>
-    <Scene
-      id='app-scene'
-      commentId='app-scene'
-      style={{
-        width: 744,
-        height: 1133,
-        position: 'absolute',
-        left: 1036,
-        top: 128,
-      }}
-      data-label='My App'
-    >
-      <App style={{}} />
-    </Scene>
-  </Storyboard>
-)
+export var storyboard = <Storyboard />
